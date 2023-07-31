81d85795f7a029378e99a7a89db3cdc93c078422
Aspirel <jonmultimedia1@gmail.com>
2023-07-31 01:45:15 +0100

    Added code to remove objects based on empty or null fields input by the user.

--- file_parser.py
+++ file_parser.py
@@ -17,14 +17,6 @@ def write_file(data, file_name):
     f.close()
 
 
-# writes all the duplicate values to a file
-def write_duplicates(data, file_name):
-    application_path = get_os_file_path()
-    with open(os.path.join(application_path, file_name), 'w', encoding='utf-8') as f:
-        json.dump(data, f, indent=4, ensure_ascii=False)
-    f.close()
-
-
 # reads a json file and parss its data
 def read_file(file_name):
     file = open(file_name, 'r', encoding="utf8")
@@ -70,13 +62,11 @@ def parse_duplicates(file_data, fields):
                     added_objects.append(item_object)
                 else:
                     duplicates.append(item)
-
-
         print('Parsing ' + str(i + 1) + ' of ' + str(len(file_data)))
 
-    write_file(result_items, 'removed_duplicates.json')
-    write_duplicates(duplicates, 'file_duplicates.json')
-    print('\nNumber of duplicates ', len(file_data) - len(result_items))
+    write_file(result_items, 'no_duplicates.json')
+    write_file(duplicates, 'duplicates.json')
+    print('\nNumber of duplicates ', len(duplicates))
     print("\nParse completed! Parsed and duplicates files have been created. It took %s seconds" %
           round((time.time() - start_time), 2))
 
@@ -110,14 +100,49 @@ def validate_search_fields(data, fields):
             menu()
 
 
+# Removes search fiel names spaces and commas
+def format_search_fields(field_names):
+    if ',' in field_names:
+        field_names = field_names.replace(' ', '')
+        field_names = field_names.split(',')
+    elif ' ' in field_names:
+        field_names = field_names.split(' ')
+
+    return field_names
+
+
+# Removes empty objects from the file based on empty fields
+def parse_empty_fields(file_data, fields):
+    print('Starting empty removal tool...')
+    start_time = time.time()
+    result_items = []
+    empty_items = []
+
+    for i, item in enumerate(file_data):
+        for field in fields:
+            if field in item:
+                if item[field] is None or len(item[field]) <= 0:
+                    if item not in empty_items:
+                        empty_items.append(item)
+                else:
+                    if item not in result_items:
+                        result_items.append(item)
+        print('Parsing ' + str(i + 1) + ' of ' + str(len(file_data)))
+
+    write_file(result_items, 'no_empties.json')
+    write_file(empty_items, 'empties.json')
+    print('\nNumber of empty items ', len(empty_items))
+    print("\nParse completed! Parsed and empties files have been created. It took %s seconds" %
+          round((time.time() - start_time), 2))
+
+
 # The user menu with options for different funtions
 def menu():
     print('Please choose an option:')
     print('1 - File length')
     print('2 - Remove duplicates - Array of objects [{}]')
     # print('3 - Remove duplicates - Json, nested arrays of objects {"example":[{}]}')
-    # print('4 - Add to file - no duplicates')
-    # print('5 - Remove from file')
+    print('4 - Remove from file based on empty fields')
 
     option = input()
     if len(option) > 0:
@@ -130,19 +155,22 @@ def menu():
             file_name = input('File name: ')
             parsed_data = validate_file(file_name)
             field_names = input('Fields to check duplicates for (comma or space separated): ')
-            if ',' in field_names:
-                field_names = field_names.replace(' ', '')
-                field_names = field_names.split(',')
-            elif ' ' in field_names:
-                field_names = field_names.split(' ')
-
+            field_names = format_search_fields(field_names)
             validate_search_fields(parsed_data, field_names)
             parse_duplicates(parsed_data, field_names)
             continue_prompt()
         elif option == '3':
             file_name = input('File name: ')
-            parsed_data = read_file(file_name)
+            parsed_data = validate_file(file_name)
             parse_nested_arrays(parsed_data)
+            continue_prompt()
+        elif option == '4':
+            file_name = input('File name: ')
+            parsed_data = validate_file(file_name)
+            field_names = input('Fields to check empty for (comma or space separated): ')
+            field_names = format_search_fields(field_names)
+            validate_search_fields(parsed_data, field_names)
+            parse_empty_fields(parsed_data, field_names)
             continue_prompt()
     else:
         print('Something went wrong')
