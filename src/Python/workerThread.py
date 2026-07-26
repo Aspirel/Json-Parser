cfe35710137002e28ceca10e0a6b0613dc72d8ed
Daniel Alves <56369053+Aspirel@users.noreply.github.com>
2026-07-26 21:05:42 +0100

    Refactor WorkerThread to use 'fn' instead of 'method'

--- src/Python/workerThread.py
+++ src/Python/workerThread.py
@@ -1,16 +1,15 @@
+# workerThread.py
 from PySide6.QtCore import QThread, Signal
 
 
 class WorkerThread(QThread):
     finished = Signal()
 
-    def __init__(self, method):
+    def __init__(self, fn):
         super().__init__()
-        self.method = method
+        self.fn = fn
+        self.result = None
 
     def run(self):
-        # Call the provided parse method in the threaded context
-        self.method()
-
-        # Emit the finished signal when the long operation is completed
+        self.result = self.fn()
         self.finished.emit()
